7f737954fb1e70e8f0fd7c8ec4f8d17b11f2d7ef
eBenya <koly_07@mail.ru>
2022-10-09 13:25:30 +0300

    Distance Based on Latitude and Longitude

--- AirApiTest/AirApiTest/Services/DistanceService.cs
+++ AirApiTest/AirApiTest/Services/DistanceService.cs
@@ -2,7 +2,9 @@
 
 public static class DistanceService
 {
-    public static dynamic Calculate(Point a, Point b)
+    const double D2R = Math.PI / 180.0;
+
+    public static dynamic CalculateTwoPointDistance(Point a, Point b)
         //where T : INumber<T>
     {
         var dXSquare = Math.Abs(Math.Pow(b.X - a.X, 2));
@@ -10,6 +12,24 @@ public static class DistanceService
 
         return Math.Sqrt(dXSquare + dYSquare);
     }
+
+    public static double CalculateTwoPointGeoDistanceInMile(Point pos1, Point pos2, DistanceType type)
+    {
+        double R = (type == DistanceType.Miles) ? 3960 : 6371;
+
+        double dLat = (pos2.Y - pos1.Y) * D2R;
+        double dLon = (pos2.X - pos1.X) * D2R;
+
+        double a = Math.Sin(dLat / 2) * Math.Sin(dLat / 2) +
+                   Math.Cos(pos1.Y * D2R) * Math.Cos(pos2.Y * D2R) *
+                   Math.Sin(dLon / 2) * Math.Sin(dLon / 2);
+        double c = 2 * Math.Asin(Math.Min(1, Math.Sqrt(a)));
+        double d = R * c;
+
+        return d;
+    }
+
+    public enum DistanceType { Miles, Kilometers };
 }
 
 public struct Point
